c9e265c46e435cee5f77013b82feefec84ad2773
Patrick Schaufuss <pajo0001@stud.ek.dk>
2025-11-21 13:28:13 +0100

    Project created

---
+++ src/test/java/org/example/projectplanner/ProjectPlannerApplicationTests.java
@@ -0,0 +1,15 @@
+package org.example.projectplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPlannerApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
